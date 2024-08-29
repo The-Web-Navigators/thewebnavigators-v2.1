2faee20596539bf85028fae16315a3693fce3014
Jatin Dahiya <jatindahiya100@gmail.com>
2024-08-29 14:55:00 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div className='font-sans font-ex'>App</div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{html,js,jsx,mjs}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Proxima Nova"', ...defaultTheme.fontFamily.sans],
+        sans: ['geistsans', ...defaultTheme.fontFamily.sans],
+      },
+      fontWeight: {
+        'ultralight': 100,
+        'light': 300,
+        'medium': 500,
+        'semibold': 600,
+        'bold': 700,
+        'ultrablack': 800,
       },
     },
   },
