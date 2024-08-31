21601db39fbb5dfb9f9048ba32c023a6c181afad
Jatin Dahiya <jatindahiya100@gmail.com>
2024-08-31 14:16:44 +0530

    Deploy

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Header from './components/Header';
 function App() {
   return (
     <ThemeProvider>
-      <Router basename="/">
+      <Router basename="/thewebnavigators-v2.1">
         <Header />
         <Routes>
           {/* Define routes */}
