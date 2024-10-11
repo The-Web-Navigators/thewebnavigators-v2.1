7b9b464e5a32f5413d024b6a9269d187cd7c9f82
Jatin Dahiya <jatindahiya100@gmail.com>
2024-10-11 21:53:50 +0530

    Updated 11-10-2024

--- src/components/react/Header.jsx
+++ src/components/react/Header.jsx
@@ -66,11 +66,11 @@ export default function Header() {
                     <Link className="w-full text-lg text-tertiary-dark dark:text-tertiary-light border-grey-dark p-4 border-b-[0.50px] border-tertiary-light dark:border-tertiary-dark" to="/what-we-do" onClick={closeNavBar}>What We Do?</Link>
                     <Link className="w-full text-lg text-tertiary-dark dark:text-tertiary-light border-grey-dark p-4 border-b-[0.50px] border-tertiary-light dark:border-tertiary-dark" to="/portfolio" onClick={closeNavBar}>Portfolio</Link>
                     <Link className="w-full text-lg text-tertiary-dark dark:text-tertiary-light border-grey-dark p-4 border-b-[0.50px] border-tertiary-light dark:border-tertiary-dark" to="/blogs" onClick={closeNavBar}>Blogs</Link>
-                    <Link className="w-full text-lg text-tertiary-dark dark:text-tertiary-light border-grey-dark p-4 border-b-[0.50px] border-tertiary-light dark:border-tertiary-dark" to="/talk-to-us" onClick={() => {
+                    <div className="w-full text-lg text-tertiary-dark dark:text-tertiary-light border-grey-dark p-4 border-b-[0.50px] border-tertiary-light dark:border-tertiary-dark" onClick={() => {
                         closeNavBar();
                         window.tidioChatApi.show();
                         window.tidioChatApi.open();
-                    }}>Talk To Us</Link>
+                    }}>Talk To Us</div>
                 </div>
             </nav>
         </>
