df65a7ca9d40b7fe54af038d9636b90f0cf5e7b2
Jatin Dahiya <jatindahiya100@gmail.com>
2024-10-12 12:56:14 +0530

    Updated 12-10-2024

--- src/components/react/MeetTheTeam.jsx
+++ src/components/react/MeetTheTeam.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Jatin from '../../assets/images/jatin.jpeg'
 import Shubhangi from '../../assets/images/shubhangi.jpeg'
 import Simran from '../../assets/images/simran.jpeg'
-import Saurabh from '../../assets/images/saurabh.jpeg'
+import Saurabh from '../../assets/images/saurabh.jpeg';
+import { ChevronRight } from "lucide-react";
+import { Link } from 'react-router-dom';
 
 const MeetTheTeam = () => {
     const teamMembers = [
@@ -35,9 +37,14 @@ const MeetTheTeam = () => {
                     <h2 className="bg-gradient-to-br from-black to-black/40 dark:from-white dark:to-white/40 bg-clip-text py-2 text-3xl font-medium leading-none tracking-tighter text-transparent sm:text-3xl md:text-4xl lg:text-5xl">
                         Meet the team
                     </h2>
-                    <p className="mb-8 text-balance text-lg font-normal tracking-tight text-gray-400">
+                    <p className="py-2 text-balance text-lg font-normal tracking-tight text-gray-400">
                         Meet the dedicated individuals who lead our team to success. Their expertise and leadership drive our company forward, ensuring that we deliver the best products and services to our customers.
                     </p>
+                    <Link to="https://www.upwork.com/freelancers/~012d6eb926a1f7a86a" target='_blank' className="py-2 border-none outline-none font-medium text-tertiary-dark dark:text-tertiary-light leading-none tracking-tight inline-flex items-center gap-1 group">
+                        View Profile
+                        <ChevronRight className="ml-1 size-3 transition-transform ease-in-out duration-300 group-hover:translate-x-1" />
+                    </Link>
+
                 </div>
                 <ul className="grid grid-cols-2 gap-x-8 gap-y-12">
                     {teamMembers.map((member, index) => (
